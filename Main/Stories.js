11c47b0a6fff571cf017ac954555ccde838da57e
Salem-Abderaouf <salamasalem84@gmail.com>
2021-02-10 23:05:36 +0100

    Update Stories

--- Main/Stories.js
+++ Main/Stories.js
@@ -9,22 +9,34 @@ const Stories = () => {
         <TouchableHighlight
           style={styles.storyBox}
           onPress={console.log('To Specifique Story')}>
-          <Text style={styles.title}>الذئب في ثوب خروف</Text>
+          <>
+            <Text style={styles.title}>الذئب في ثوب خروف</Text>
+            <Text style={styles.subTitle}>45 دقيقة</Text>
+          </>
         </TouchableHighlight>
         <TouchableHighlight
-          style={styles.storyBox}
+          style={[styles.storyBox, { backgroundColor: Colors.SecondList }]}
           onPress={console.log('To Specifique Story')}>
-          <Text style={styles.title}>النملة والحمامة</Text>
+          <>
+            <Text style={styles.title}>النملة والحمامة</Text>
+            <Text style={styles.subTitle}>25 دقيقة</Text>
+          </>
         </TouchableHighlight>
         <TouchableHighlight
-          style={styles.storyBox}
+          style={[styles.storyBox, { backgroundColor: Colors.Primary }]}
           onPress={console.log('To Specifique Story')}>
-          <Text style={styles.title}>النملة والحمامة</Text>
+          <>
+            <Text style={styles.title}> جميلة والوحش</Text>
+            <Text style={styles.subTitle}>40 دقيقة</Text>
+          </>
         </TouchableHighlight>
         <TouchableHighlight
-          style={styles.storyBox}
+          style={[styles.storyBox, { backgroundColor: Colors.FirstList }]}
           onPress={console.log('To Specifique Story')}>
-          <Text style={styles.title}>النملة والحمامة</Text>
+          <>
+            <Text style={styles.title}> المحارب الصغير</Text>
+            <Text style={styles.subTitle}>30 دقيقة</Text>
+          </>
         </TouchableHighlight>
       </ScrollView>
     </View>
@@ -37,16 +49,23 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
   },
   storyBox: {
+    padding: 20,
     height: 150,
     marginVertical: 10,
     width: Dimensions.get('window').width - 60,
-    backgroundColor: Colors.Primary,
+    backgroundColor: Colors.FirstList,
     borderRadius: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
   title: {
     fontSize: 22,
+    fontWeight: "bold",
+    color: Colors.White,
+  }, subTitle: {
+    fontSize: 16,
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
     color: Colors.White,
   }
 })
