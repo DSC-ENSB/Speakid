7e8e7f34060551ee1caf09e1962a18d963c69123
Salem-Abderaouf <salamasalem84@gmail.com>
2021-03-10 13:49:09 +0100

    Push to DB

--- Components/Questions.js
+++ Components/Questions.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import database from '@react-native-firebase/database';
+import auth from '@react-native-firebase/auth'
 import Colors from "../style/Colors";
 
 
 const Questions = (props) => {
   const [isFalseEnabled, setIsFalseEnabled] = useState(false);
-
-  const togglSwitch = (id) => {
-    console.log(id)
+  const [answer, setAnswer] = useState(null)
+  const userID = auth().currentUser.uid
+  const dbRef = database().ref(`user/${userID}/answers`)
+  const togglSwitch = async (id) => {
     const SelectedOption = props.answers.filter(el => props.answers.indexOf(el) === id);
+    await setAnswer({ [props.title]: SelectedOption })
+    dbRef.update(answer)
     setIsFalseEnabled(!isFalseEnabled)
   };
 
