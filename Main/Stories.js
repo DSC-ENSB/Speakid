b60fac696949dc8a31b8b2486b4f7e90264c4be8
Salem-Abderaouf <salamasalem84@gmail.com>
2021-03-12 00:11:35 +0100

    uUpdate stories component

--- Main/Stories.js
+++ Main/Stories.js
@@ -1,45 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet, TouchableHighlight, Dimensions, ScrollView } from "react-native";
+import { SwipeablePanel } from 'rn-swipeable-panel';
+import Stors from './Storiesdata'
 import Colors from '../style/Colors';
 
 const Stories = () => {
+
+  const [panelProps, setPanelProps] = useState({
+    fullWidth: true,
+    openLarge: true,
+    showCloseButton: true,
+    onClose: () => closePanel(),
+    onPressCloseButton: () => closePanel(),
+  });
+  const [isPanelActive, setIsPanelActive] = useState(false);
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const openPanel = () => {
+    setIsPanelActive(true);
+  };
+
+  const closePanel = () => {
+    setIsPanelActive(false);
+  };
+  const openStory = id => {
+    setTitle(Stors[id].title)
+    setBody(Stors[id].body)
+    openPanel()
+  }
+
   return (
-    <View style={styles.wrapper}>
-      <ScrollView>
-        <TouchableHighlight
-          style={styles.storyBox}
-          onPress={console.log('To Specifique Story')}>
-          <>
-            <Text style={styles.title}>الذئب في ثوب خروف</Text>
-            <Text style={styles.subTitle}>45 دقيقة</Text>
-          </>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={[styles.storyBox, { backgroundColor: Colors.SecondList }]}
-          onPress={console.log('To Specifique Story')}>
-          <>
-            <Text style={styles.title}>النملة والحمامة</Text>
-            <Text style={styles.subTitle}>25 دقيقة</Text>
-          </>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={[styles.storyBox, { backgroundColor: Colors.Primary }]}
-          onPress={console.log('To Specifique Story')}>
-          <>
-            <Text style={styles.title}> جميلة والوحش</Text>
-            <Text style={styles.subTitle}>40 دقيقة</Text>
-          </>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={[styles.storyBox, { backgroundColor: Colors.FirstList }]}
-          onPress={console.log('To Specifique Story')}>
-          <>
-            <Text style={styles.title}> المحارب الصغير</Text>
-            <Text style={styles.subTitle}>30 دقيقة</Text>
-          </>
-        </TouchableHighlight>
-      </ScrollView>
-    </View>
+    <>
+      <View style={styles.wrapper}>
+        <SwipeablePanel {...panelProps} isActive={isPanelActive}>
+          <View style={styles.storiesHandler}>
+            <Text style={styles.Storytitle}>{title}</Text>
+            <ScrollView>
+              <Text style={styles.body}>{body}</Text>
+            </ScrollView>
+          </View>
+        </SwipeablePanel>
+        <ScrollView>
+          <TouchableHighlight
+            style={styles.storyBox}
+            onPress={() => openStory("1")}>
+            <>
+              <Text style={styles.title}>الذئب في ثوب خروف</Text>
+              <Text style={styles.subTitle}>25 دقيقة</Text>
+            </>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={[styles.storyBox, { backgroundColor: Colors.SecondList }]}
+            onPress={() => openStory("2")}>
+            <>
+              <Text style={styles.title}>النملة والحمامة</Text>
+              <Text style={styles.subTitle}>45 دقيقة</Text>
+            </>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={[styles.storyBox, { backgroundColor: Colors.Primary }]}
+            onPress={() => openStory("3")}>
+            <>
+              <Text style={styles.title}> التضحية من أجل صديق </Text>
+              <Text style={styles.subTitle}>40 دقيقة</Text>
+            </>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={[styles.storyBox, { backgroundColor: Colors.FirstList }]}
+            onPress={() => openStory("4")}>
+            <>
+              <Text style={styles.title}> فكر قبل أن تتكلم</Text>
+              <Text style={styles.subTitle}>45 دقيقة</Text>
+            </>
+          </TouchableHighlight>
+        </ScrollView>
+      </View>
+    </>
   )
 }
 const styles = StyleSheet.create({
@@ -61,12 +98,25 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "bold",
     color: Colors.White,
-  }, subTitle: {
+  },
+  subTitle: {
     fontSize: 16,
     position: 'absolute',
     bottom: 20,
     left: 20,
     color: Colors.White,
+  }, storiesHandler: {
+    padding: 25,
+  },
+  Storytitle: {
+    textAlign: "center",
+    fontSize: 25,
+    fontWeight: "bold",
+    color: Colors.Black,
+    marginBottom: 10,
+  }, body: {
+    fontSize: 17,
+    lineHeight: 30,
   }
 })
 export default Stories
